e0160a88efec7d9b0dc66bf591854438defd1862
Nowath <nanoprocom@gmail.com>
2023-04-21 14:44:00 +0700

    New

---
+++ script/app.js
@@ -0,0 +1,36 @@
+new FinisherHeader({
+    "count": 12,
+    "size": {
+        "min": 1300,
+        "max": 1500,
+        "pulse": 0
+    },
+    "speed": {
+        "x": {
+            "min": 0.6,
+            "max": 3
+    },
+        "y": {
+            "min": 0.6,
+            "max": 3
+    }
+    },
+    "colors": {
+        "background": "#8231e6",
+        "particles": [
+            "#ff681c",
+            "#ffe895",
+            "#fba64a",
+            "#ff0a53"
+        ]
+    },
+    "blending": "lighten",
+    "opacity": {
+        "center": 0.6,
+        "edge": 0
+    },
+    "skew": 0,
+    "shapes": [
+        "c"
+    ]
+});
